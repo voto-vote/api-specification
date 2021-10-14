4d7a37ae8d456320700b0df99c8ff73ed82f56d2
Simon Strobel <simon@codebrew.de>
2021-10-14 19:18:36 +0200

    [OVAAI] Added WIP

--- openapi/docker-entrypoint.sh
+++ openapi/docker-entrypoint.sh
@@ -4,9 +4,9 @@
 mkdir -p output/generated
 
 echo "Merging files..."
-speccy resolve ./specification.yaml -o output/spec-merged.yml
+speccy resolve ./specification.yaml -o output/spec-merged.yaml
 
 cd  output
 
 echo "Generate code..."
-openapi-generator-cli generate -i ./spec-merged.yml -g $LANGUAGE -o ./generated/
+openapi-generator-cli generate -i ./spec-merged.yaml -g $LANGUAGE -o ./generated/
