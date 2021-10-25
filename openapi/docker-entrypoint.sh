76a0308ad41dc86215ba3c7c34f7a3a03df9325a
strobsi <simon@codebrew.de>
2021-10-25 10:40:49 +0200

    [VOTO] Fixed minor stuff (#3)
    * [OVAAI] Added draft progress
    
    * [OVAAI] Added draft progress
    
    * [OVAAI] Added draf progress
    
    * [OVAAI] Added draft links
    
    * [OVAAI] Added draft readme
    
    * [CODEBREW] Dump SiWabe code to Gitlab
    
    * Initial commit from create-openapi-repo
    
    * [OVAAI] Added WIP
    
    * [OVAAI] Added WIP
    
    * [OVAAI] Added WIP
    
    * [OVAAI] Added WIP
    
    * [OVAAI] Added WIP
    
    * [OVAAI] Added WIP
    
    * [OVAAI] Added github page
    
    * [OVAAI] Added github page
    
    * [OVAAI] Added github page
    
    * [OVAAI] Added github page
    
    * [VOTO] Added security authentication
    
    * [VOTO] Added security scheme
    
    * [VOTO] Fixed readme
    
    * [VOTO] Fixed readme
    
    * [VOTO] Fixed readme
    
    * [VOTO] Added description of RBAC
    
    * [VOTO] Added more responses
    
    * [VOTO] Added examples to error description
    
    * [VOTO] Use version 5.2.1 of generator

--- openapi/docker-entrypoint.sh
+++ openapi/docker-entrypoint.sh
@@ -12,5 +12,7 @@ npx @redocly/openapi-cli bundle specification.yaml -o ./output/spec-merged.yaml
 
 cd  output
 
+openapi-generator-cli version-manager set 5.2.1
+
 echo "Generate code..."
-openapi-generator-cli generate -i ./spec-merged.yaml -g $LANGUAGE -o ./generated/
+openapi-generator-cli generate -i ./spec-merged.yaml -g $LANGUAGE -o ./generated/ --verbose
